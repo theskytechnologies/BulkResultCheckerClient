4b96f19e59a8efe32db65cafbeec5477688e3f6e
bharatbullstreet <bharatbullstreet@gmail.com>
2025-05-07 01:06:45 +0530

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { AutoResultCheckerComponent } from './components/auto-result-checker/auto-result-checker.component';
-import { HomeComponent } from './layout/home/home.component';
-
-export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    // You can add additional routes here if needed
-  ];
